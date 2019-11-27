10070cd784b05f4cac05e0f43b5c20dc5d092a26
Arthur Lazarenko <Arthur.Lazarenko@icloud.com>
2019-11-27 11:57:31 +0300

    Правка скриптов запуска. Убраны лишние файлы из репозитория.

--- .build/getConfig.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = require(process.env.CONFIG_PATH || "../config.json");
-exports.default = () => config;
-//# sourceMappingURL=getConfig.js.map
