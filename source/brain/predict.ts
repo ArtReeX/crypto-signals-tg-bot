9579fbc8ccd17ac8674d428573a918964d09c125
Arthur Lazarenko <Arthur.Lazarenko@icloud.com>
2019-12-27 22:04:12 +0300

    Правка логики работы.

--- source/brain/predict.ts
+++ source/brain/predict.ts
@@ -3,14 +3,9 @@ import { ICandle } from "../binance/getHistory";
 import { toArray } from "./utilities";
 import { denormalize, normalize, smooth } from "./normalization";
 
-export default (
-  model: tf.LayersModel,
-  candles: ICandle[],
-  sequence: number
-): number[] => {
+export default (model: tf.LayersModel, candles: ICandle[]): number[] => {
   const array = candles.map(c => toArray(c));
-  const seq = array.slice(candles.length - sequence, candles.length);
-  const tensor = tf.tensor3d([seq]);
+  const tensor = tf.tensor3d([array]);
 
   const result = model.predict(tensor) as tf.Tensor2D;
   const [pick] = result.arraySync() as number[][];
--- source/tracking.ts
+++ source/tracking.ts
@@ -4,18 +4,30 @@ import bot from "./bot";
 import brain from "./brain";
 import { IDirections } from "./config";
 
+const lastPrices: { [key: string]: number } = {};
+
 export default async (
   model: tf.LayersModel,
   directions: IDirections,
   sequence: number
 ) => {
   for (let symbol in directions) {
-    for (let interval of directions[symbol].intervals) {
+    for (let interval of directions[symbol]) {
       try {
         const candles = await binance.getHistory(symbol, interval);
-        const result = brain.predict(model, candles, sequence);
+        const seq = candles.slice(candles.length - sequence, candles.length);
 
-        console.log(result);
+        const result = brain.predict(model, seq);
+
+        if (lastPrices[symbol + interval] !== result[1]) {
+          bot.sendMessage(
+            `[${symbol}/${interval}] - ${result[1]}, last -> ${
+              result[1] > lastPrices[symbol + interval] ? "+" : "-"
+            }`
+          );
+
+          lastPrices[symbol + interval] = result[1];
+        }
       } catch ({ message }) {
         console.error(
           `Failed to get direction information ${symbol} with interval ${interval}: ${message}.`
